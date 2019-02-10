173fd532600f4c25efd9eb0885fc14b52cf1e47e
Ali Abdulhussein <abdulhussein.ali@gmail.com>
2019-02-10 11:03:31 -0800

    Add new implementation project, Uncomment the implementation

--- Wise-It/LongestSequenceNewImplementation/Program.cs
+++ Wise-It/LongestSequenceNewImplementation/Program.cs
@@ -1,73 +1,74 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LongestSequence;
 
 namespace LongestSequenceNewImplementation
 {
     class Program
     {
+        private static int totalNumberDays = 0;
+        private static int startIndex;
+        private static int endIndex;
+        private static int start;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitSequence();
+            Console.WriteLine("Result: {0} {1}", GetStartDay(), GetEndDay());
+
             Console.ReadKey();
 
 
-            /**
-             * using System; 
-             * public class Solution : ISolution 
-             * { 
-             * int totalNumberDays = 0; 
-             * int startIndex; int endIndex; 
-             * int start; 
-             * public Solution() { 
-             * // You can initiate and calculate things here 
-             * totalNumberDays = API.GetNumDays(); 
-             * LongestStreak(totalNumberDays); 
-             * } 
-             * 
-             * void LongestStreak(int totalNumberDays) { 
-             * int max = 0; 
-             * int count = 1; 
-             * startIndex = 0; 
-             * endIndex = 0; 
-             * for (int i = 0; i < totalNumberDays - 2; i++) { 
-             * startIndex = i; 
-             * while (i < totalNumberDays - 1 && Check(i)) { i++; count++; } 
-             * if (max < count) { 
-             * max = count; 
-             * start = startIndex; 
-             * endIndex = i; count = 1; 
-             * } 
-             * } 
-             * } 
-             * 
-             * bool Check(int index) { 
-             * for (int i = startIndex; i <= index; i++) { 
-             * int temp = API.GetTemperatureOnDay(i); 
-             * for (int j = i + 1; j <= index; j++) { 
-             * int nexTemp = API.GetTemperatureOnDay(j ); 
-             * if (Math.Abs(temp - nexTemp) > 5) 
-             * return false; 
-             * } 
-             * } 
-             * return true; 
-             * } 
-             * 
-             * /** * Return the start day for the longest sequence of days where the mean temperature * is within five degrees.
-             * public int GetStartDay()
-             * { 
-             * // Write your code here 
-             * return start; 
-             * } 
-             * 
-             * /** * Return the end day for the longest sequence of days where the mean temperature is * within five degrees. Note that the start and end day of the sequence may be the * same.
-             * public int GetEndDay() { 
-             * // Write your code here return 
-             * endIndex; 
-             * }
-             * */
+        }
+
+
+        public static void InitSequence()
+        {
+            totalNumberDays = ApiClass.GetNumDays();
+            LongestStreak(totalNumberDays);
+        }
+
+        public static void LongestStreak(int totalNumberDays)
+        {
+            int max = 0;
+            int count = 1;
+            startIndex = 0;
+            endIndex = 0;
+            for (int i = 0; i < totalNumberDays - 2; i++)
+            {
+                startIndex = i;
+                while (i < totalNumberDays - 1 && Check(i)) { i++; count++; }
+                if (max < count)
+                {
+                    max = count;
+                    start = startIndex;
+                    endIndex = i; count = 1;
+                    }
+                }
+        }
+
+        public static bool Check(int index)
+        {
+            for (int i = startIndex; i <= index; i++)
+            {
+                int temp = ApiClass.GetTemperatureOnDay(i);
+                for (int j = i + 1; j <= index; j++)
+                {
+                    int nexTemp = ApiClass.GetTemperatureOnDay(j);
+                        if (Math.Abs(temp - nexTemp) > 5)
+                            return false;
+                }
             }
+            return true;
+        }
+
+        public static int GetStartDay()
+        {
+            return start;
+        }
+
+        public static int GetEndDay()
+        {
+            return endIndex;
+        }
     }
 }

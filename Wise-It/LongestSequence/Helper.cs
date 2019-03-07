cedc033cfb66795122e2ad31af49b5100f286877
Ali Abdulhussein <abdulhussein.ali@gmail.com>
2019-03-07 20:42:00 +0100

    Change NameSpace name

--- Wise-It/LongestSequence/Helper.cs
+++ Wise-It/LongestSequence/Helper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LongestSequence
+namespace Hackerrank
 {
     public class ListTypeClass<T> : List<T>
     {
@@ -183,4 +183,6 @@ namespace LongestSequence
             return (checkvalue < 3 && grade >= 38);
         }
     }
+
+
 }
--- Wise-It/LongestSequenceNewImplementation/Program.cs
+++ Wise-It/LongestSequenceNewImplementation/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using LongestSequence;
+using Hackerrank;
 
 namespace LongestSequenceNewImplementation
 {
